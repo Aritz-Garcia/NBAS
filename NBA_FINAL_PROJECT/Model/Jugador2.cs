0317c6e78c60ea331274a573e98de51773194fd5
Agerzace <ager.zabalace@elorrieta-errekamari.com>
2023-02-23 09:14:12 +0100

    clase nueva (Jugador 2)

---
+++ NBA_FINAL_PROJECT/Model/Jugador2.cs
@@ -0,0 +1,224 @@
+﻿namespace NBA_FINAL_PROJECT.Model
+{
+
+    public class Jugador2
+    {
+        public _Internal _internal { get; set; }
+        public League league { get; set; }
+    }
+
+    public class _Internal
+    {
+        public string pubDateTime { get; set; }
+        public string igorPath { get; set; }
+        public string xslt { get; set; }
+        public string xsltForceRecompile { get; set; }
+        public string xsltInCache { get; set; }
+        public string xsltCompileTimeMillis { get; set; }
+        public string xsltTransformTimeMillis { get; set; }
+        public string consolidatedDomKey { get; set; }
+        public string endToEndTimeMillis { get; set; }
+    }
+
+    public class League
+    {
+        public Standard[] standard { get; set; }
+        public object[] africa { get; set; }
+        public Sacramento[] sacramento { get; set; }
+        public Vega[] vegas { get; set; }
+        public Utah[] utah { get; set; }
+    }
+
+    public class Standard
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string temporaryDisplayName { get; set; }
+        public string personId { get; set; }
+        public string teamId { get; set; }
+        public string jersey { get; set; }
+        public bool isActive { get; set; }
+        public string pos { get; set; }
+        public string heightFeet { get; set; }
+        public string heightInches { get; set; }
+        public string heightMeters { get; set; }
+        public string weightPounds { get; set; }
+        public string weightKilograms { get; set; }
+        public string dateOfBirthUTC { get; set; }
+        public Teamsitesonly teamSitesOnly { get; set; }
+        public Team[] teams { get; set; }
+        public Draft draft { get; set; }
+        public string nbaDebutYear { get; set; }
+        public string yearsPro { get; set; }
+        public string collegeName { get; set; }
+        public string lastAffiliation { get; set; }
+        public string country { get; set; }
+        public bool isallStar { get; set; }
+    }
+
+    public class Teamsitesonly
+    {
+        public string playerCode { get; set; }
+        public string posFull { get; set; }
+        public string displayAffiliation { get; set; }
+        public string freeAgentCode { get; set; }
+    }
+
+    public class Draft
+    {
+        public string teamId { get; set; }
+        public string pickNum { get; set; }
+        public string roundNum { get; set; }
+        public string seasonYear { get; set; }
+    }
+
+    public class Team2
+    {
+        public string teamId { get; set; }
+        public string seasonStart { get; set; }
+        public string seasonEnd { get; set; }
+    }
+
+    public class Sacramento
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string temporaryDisplayName { get; set; }
+        public string personId { get; set; }
+        public string teamId { get; set; }
+        public string jersey { get; set; }
+        public bool isActive { get; set; }
+        public string pos { get; set; }
+        public string heightFeet { get; set; }
+        public string heightInches { get; set; }
+        public string heightMeters { get; set; }
+        public string weightPounds { get; set; }
+        public string weightKilograms { get; set; }
+        public string dateOfBirthUTC { get; set; }
+        public Teamsitesonly1 teamSitesOnly { get; set; }
+        public Team1[] teams { get; set; }
+        public Draft1 draft { get; set; }
+        public string nbaDebutYear { get; set; }
+        public string yearsPro { get; set; }
+        public string collegeName { get; set; }
+        public string lastAffiliation { get; set; }
+        public string country { get; set; }
+    }
+
+    public class Teamsitesonly1
+    {
+        public string playerCode { get; set; }
+        public string posFull { get; set; }
+        public string displayAffiliation { get; set; }
+        public string freeAgentCode { get; set; }
+    }
+
+    public class Draft1
+    {
+        public string teamId { get; set; }
+        public string pickNum { get; set; }
+        public string roundNum { get; set; }
+        public string seasonYear { get; set; }
+    }
+
+    public class Team12
+    {
+        public string teamId { get; set; }
+        public string seasonStart { get; set; }
+        public string seasonEnd { get; set; }
+    }
+
+    public class Vega
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string temporaryDisplayName { get; set; }
+        public string personId { get; set; }
+        public string teamId { get; set; }
+        public string jersey { get; set; }
+        public bool isActive { get; set; }
+        public string pos { get; set; }
+        public string heightFeet { get; set; }
+        public string heightInches { get; set; }
+        public string heightMeters { get; set; }
+        public string weightPounds { get; set; }
+        public string weightKilograms { get; set; }
+        public string dateOfBirthUTC { get; set; }
+        public Teamsitesonly2 teamSitesOnly { get; set; }
+        public Team2[] teams { get; set; }
+        public Draft2 draft { get; set; }
+        public string nbaDebutYear { get; set; }
+        public string yearsPro { get; set; }
+        public string collegeName { get; set; }
+        public string lastAffiliation { get; set; }
+        public string country { get; set; }
+        public bool isallStar { get; set; }
+    }
+
+    public class Teamsitesonly2
+    {
+        public string playerCode { get; set; }
+        public string posFull { get; set; }
+        public string displayAffiliation { get; set; }
+        public string freeAgentCode { get; set; }
+    }
+
+    public class Draft2
+    {
+        public string teamId { get; set; }
+        public string pickNum { get; set; }
+        public string roundNum { get; set; }
+        public string seasonYear { get; set; }
+    }
+
+    public class Team22
+    {
+        public string teamId { get; set; }
+        public string seasonStart { get; set; }
+        public string seasonEnd { get; set; }
+    }
+
+    public class Utah
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string temporaryDisplayName { get; set; }
+        public string personId { get; set; }
+        public string teamId { get; set; }
+        public string jersey { get; set; }
+        public bool isActive { get; set; }
+        public string pos { get; set; }
+        public string heightFeet { get; set; }
+        public string heightInches { get; set; }
+        public string heightMeters { get; set; }
+        public string weightPounds { get; set; }
+        public string weightKilograms { get; set; }
+        public string dateOfBirthUTC { get; set; }
+        public Teamsitesonly3 teamSitesOnly { get; set; }
+        public object[] teams { get; set; }
+        public Draft3 draft { get; set; }
+        public string nbaDebutYear { get; set; }
+        public string yearsPro { get; set; }
+        public string collegeName { get; set; }
+        public string lastAffiliation { get; set; }
+        public string country { get; set; }
+        public bool isallStar { get; set; }
+    }
+
+    public class Teamsitesonly3
+    {
+        public string playerCode { get; set; }
+        public string posFull { get; set; }
+        public string displayAffiliation { get; set; }
+        public string freeAgentCode { get; set; }
+    }
+
+    public class Draft3
+    {
+        public string teamId { get; set; }
+        public string pickNum { get; set; }
+        public string roundNum { get; set; }
+        public string seasonYear { get; set; }
+    }
+
+}
