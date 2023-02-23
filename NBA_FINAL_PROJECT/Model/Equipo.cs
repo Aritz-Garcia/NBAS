297e998e914fcdde1e1e0ce826baccf41c6cb265
Xabi224 <xabier.torrego@elorrieta-errekamari.com>
2023-02-23 11:49:43 +0100

    Cambios 4 (Equipos Info)

---
+++ NBA_FINAL_PROJECT/Model/Equipo.cs
@@ -0,0 +1,17 @@
+﻿namespace NBA_FINAL_PROJECT.Model
+{
+
+
+    public class Equipo
+    {
+        public int id { get; set; }
+        public string abbreviation { get; set; }
+        public string city { get; set; }
+        public string conference { get; set; }
+        public string division { get; set; }
+        public string full_name { get; set; }
+        public string name { get; set; }
+    }
+
+
+}
