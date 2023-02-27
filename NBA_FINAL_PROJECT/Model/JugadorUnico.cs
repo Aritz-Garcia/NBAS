1987bc9a4c4db4470244344a6c233126019bee62
Agerzace <ager.zabalace@elorrieta-errekamari.com>
2023-02-27 22:45:53 +0100

    cambios 7

---
+++ NBA_FINAL_PROJECT/Model/JugadorUnico.cs
@@ -0,0 +1,27 @@
+﻿namespace NBA_FINAL_PROJECT.Model
+{
+
+    public class JugadorUnico
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public int? height_feet { get; set; }
+        public int? height_inches { get; set; }
+        public string last_name { get; set; }
+        public string? position { get; set; }
+        public Team4 team { get; set; }
+        public int? weight_pounds { get; set; }
+    }
+
+    public class Team4
+    {
+        public int id { get; set; }
+        public string abbreviation { get; set; }
+        public string city { get; set; }
+        public string conference { get; set; }
+        public string division { get; set; }
+        public string full_name { get; set; }
+        public string name { get; set; }
+    }
+
+}
