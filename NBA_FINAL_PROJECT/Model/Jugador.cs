86df0d584de8281c64bfd351c5ece9574b9fb33c
Aritz-Garcia <73273260+Aritz-Garcia@users.noreply.github.com>
2023-03-08 00:03:27 +0100

    Jugadores info

--- NBA_FINAL_PROJECT/Model/Jugador.cs
+++ NBA_FINAL_PROJECT/Model/Jugador.cs
@@ -11,7 +11,7 @@
     {
         public int total_pages { get; set; }
         public int current_page { get; set; }
-        public int next_page { get; set; }
+        public int? next_page { get; set; }
         public int per_page { get; set; }
         public int total_count { get; set; }
     }
