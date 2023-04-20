d601a574339ec1383cd57106d93cd0642275d306
Aritz-Garcia <aritz2030@gmail.com>
2023-04-20 13:28:24 +0200

    información equipo

---
+++ NBA_FINAL_PROJECT/Model/JugadorUnicoEquipo.cs
@@ -0,0 +1,9 @@
+﻿namespace NBA_FINAL_PROJECT.Model
+{
+    public class JugadorUnicoEquipo
+    {
+        public string id { get; set; }
+        public string full_name { get; set; }
+        public int dorsal { get; set; }
+    }
+}
