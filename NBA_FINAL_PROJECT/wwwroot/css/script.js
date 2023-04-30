09095f8dc2292d376755c952c7ba47087c39c778
Aritz-Garcia <73273260+Aritz-Garcia@users.noreply.github.com>
2023-04-30 21:41:12 +0200

    mapa coordenadas y nombres

--- NBA_FINAL_PROJECT/wwwroot/css/script.js
+++ NBA_FINAL_PROJECT/wwwroot/css/script.js
@@ -1,24 +1,26 @@
-function initialize(coordenadas, nombre) {
+function initialize(coordenadas, nombre, estadio, inicio) {
     mapboxgl.accessToken = 'pk.eyJ1IjoiYWdhcmNpYWVsbyIsImEiOiJjbGgwYXBkZ2UwcnI4M3BxY2V1cThlZGVtIn0.zpbXw-zZVNEZcG3aAJTA3Q';
     const map = new mapboxgl.Map({
         container: 'map', // container ID
         // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
         style: 'mapbox://styles/mapbox/streets-v11', // style URL
-        center: [-101.46394314630489, 39.97724130613545], // starting position [lng, lat]
-        zoom: 5 // starting zoom
+        center: [inicio[1], inicio[0]], // starting position [lng, lat]
+        zoom: 4.5 // starting zoom
     });
 
-    for (var i = 0; i < nombre.length; i++) {
+    for (var i = 0; i < (coordenadas.length / 2); i++) {
 
         var num1 = i * 2;
         var num2 = i * 2 + 1;
         
-        let lng = coordenadas[num1];
-        let lat = coordenadas[num2];
+        let lat = coordenadas[num1];
+        let lng = coordenadas[num2];
 
         var element = document.createElement('div');
         element.className = nombre[i];
 
-        var marcador = new mapboxgl.Marker(element).setLngLat({ lng, lat }).addTo(map);
+        var popup = new mapboxgl.Popup().setText(estadio[i]);
+
+        var marcador = new mapboxgl.Marker(element).setLngLat({ lng, lat }).setPopup(popup).addTo(map);
     }
 }
